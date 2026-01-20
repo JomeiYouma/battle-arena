bece0e7d628948c5151e928c3cb0f2a51b75c98d
Youma <zoor@netcourrier.com>
2026-01-20 11:35:43 +0100

    Export import tests

---
+++ src/script.js
@@ -0,0 +1,7 @@
+import {test2} from './test.js';
+
+// Or you can use the default import
+
+import myFunction from './test.js';    
+
+myFunction.test2();
---
+++ src/test.js
@@ -0,0 +1,8 @@
+const test2 = () => {
+    console.log("This is test2 function");
+};
+
+//--
+export { test2 };
+
+export default { test2};
